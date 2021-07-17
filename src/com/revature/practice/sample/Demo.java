7d74b2f4eb55ee768c7e15e3291723eb73b7c510
kumar <kumar@LAPTOP-B3LOJUKS>
2021-07-17 21:02:34 +0530

    - message2

--- src/com/revature/practice/sample/Demo.java
+++ src/com/revature/practice/sample/Demo.java
@@ -1,9 +1,18 @@
 package com.revature.practice.sample;
 
 public class Demo {
+	public void show()
+	{
+		System.out.println("Hi");
+	}
 	public static void main(String[] args)
 	{
+		Demo d=new Demo();
+		d.show();
 		System.out.println("Hello");
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
 	}
 
 }
